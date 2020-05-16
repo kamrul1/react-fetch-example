45385a0037b2591de16d4383571696e12673527b
kamrul1 <kamrul01@yahoo.co.uk>
2020-05-16 19:48:31 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+
+export default props => {
+  const [items, setItems] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(json => {
+        setItems(json);
+        setIsLoaded(true);
+      });
+  });
+
+  //not sure this is working.
+
+  const apiReturn = () => {
+    if (isLoaded) {
+      return (
+        <div>
+          <ul>
+            {items.map(item => (
+              <li key={item.id}>{item.name}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    } else {
+      return <div>Loading....</div>;
+    }
+  };
+
+  return apiReturn();
+};
